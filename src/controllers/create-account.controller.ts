ee4fca8c50f08083fa709c52bcad3e75bcc5fff9
ismael <maelio@live.com>
2024-06-01 16:09:25 -0300

    Cria validação de dados usando o Zod.
    Criei o ZodValidationPipe para validar os dados de entrada em um controller. Além disso, usei a biblioteca ZodValidationError, que pode ser usada para formatar os erros de validação de uma maneira mais legível.

--- src/controllers/create-account.controller.ts
+++ src/controllers/create-account.controller.ts
@@ -1,14 +1,22 @@
-import { ConflictException } from '@nestjs/common'
+import { ConflictException, UsePipes } from '@nestjs/common'
 import { Body, Controller, HttpCode, Post } from '@nestjs/common'
 import { PrismaService } from 'src/prisma/prisma.service'
 import { hash } from 'bcryptjs'
-
+import { z } from 'zod'
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
+const createAccountBodySchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string().min(6),
+})
+type createAccountBodySchema = z.infer<typeof createAccountBodySchema>
 @Controller('/accounts')
 export class CreateAccountController {
   constructor(private prisma: PrismaService) {}
   @Post()
   @HttpCode(201)
-  async handle(@Body() body: any) {
+  @UsePipes(new ZodValidationPipe(createAccountBodySchema))
+  async handle(@Body() body: createAccountBodySchema) {
     const { name, email, password } = body
     const userWithSameEmail = await this.prisma.user.findUnique({
       where: {
