aad992638113c9fd57eb8a80a903d088fb7e7182
ismael <maelio@live.com>
2024-06-03 20:57:01 -0300

    Cria mais dois testes e2e para criar pergunstas e listar pergunstas
    Criado mais dois testes e2e, onde validamos a criação de uma pergunta de ponta a ponta e a listagem das perguntas.

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import { envSchema } from './env'
 import { AuthModule } from './auth/auth.module'
 import { AuthenticateController } from './controllers/authenticate.controller'
 import { CreateQuestionController } from './controllers/create-question.controller'
-import { FetchRecenteQuestionController } from './controllers/fetch-recent-questions'
+import { FetchRecenteQuestionController } from './controllers/fetch-recent-questions.controller'
 
 @Module({
   imports: [
